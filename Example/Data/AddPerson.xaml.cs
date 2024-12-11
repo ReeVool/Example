004879bd73505aa522230dd8f79904732b9f6de2
ReeVool <abacaraevdzamal@gmail.com>
2024-12-11 17:45:15 +0300

    Добавьте файлы проекта.

---
+++ Example/Data/AddPerson.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Example.Data
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPerson.xaml
+    /// </summary>
+    public partial class AddPerson : Window
+    {
+        public AddPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void AddPersonButton_Click(object sender, RoutedEventArgs e)
+        {
+            Persons person = new Persons();
+
+
+            if (int.TryParse(ageBox.Text, out int age))
+            {
+                person.ФИО = nameBox.Text;
+                person.Возраст = age;
+                person.Адрес = adresBox.Text;
+                person.Трудоустроен = workBox.IsChecked;
+
+                AppData.db.Persons.Add(person);
+                AppData.db.SaveChanges();
+
+                MessageBox.Show(
+                    "Данные сохранены", "Уведомление",
+                    MessageBoxButton.OK, MessageBoxImage.Information
+                );
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Введены неправильные данные.\n" +
+                    "Проверьте данные на правильность и попытайтесь снова", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error
+                );
+            }
+        }
+    }
+}
---
+++ Example/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Example.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = AppData.db.Persons.ToList();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            AddPerson addPerson = new AddPerson();
+            addPerson.ShowDialog();
+        }
+
+        private void Reload_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = AppData.db.Persons.ToList();
+        }
+
+        private void DelPerson_Click(object sender, RoutedEventArgs e)
+        {
+            var selItem = dataGrid.SelectedItem as Persons;
+            
+            if (selItem != null)
+            {
+                MessageBoxResult res = MessageBox.Show(
+                    "Вы точно хотите удалить данные?\n" +
+                    "Данное действие нельзя будет отменить.", "Подтверждение действия",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question
+                );
+
+                if (res == MessageBoxResult.Yes)
+                {
+                    AppData.db.Persons.Remove(selItem);
+                    AppData.db.SaveChanges();
+                    dataGrid.ItemsSource = AppData.db.Persons.ToList();
+                }
+            }
+
+            else
+            {
+                MessageBoxResult res = MessageBox.Show(
+                    "Не выбрана ни одна строка.\n" +
+                    "Выберите строку для удаления и повторите попытку", "Ошибка",
+                    MessageBoxButton.YesNo, MessageBoxImage.Error
+                );
+            }
+        }
+    }
+}
